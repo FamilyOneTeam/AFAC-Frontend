fe455ce72cd73bcbbc234ff7081fd6d6812f4b28
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-05 17:59:31 +0300

    feat: login route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,20 @@
+import App from '@/App';
+import Login from '@/features/auth/login/page';
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 const router = createBrowserRouter([
-    {
-    path: '/',
-    element: <></>
-  }
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/auth/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <h1>Página no encontrada</h1>,
+  },
 ]);
 
 export default function Router() {
