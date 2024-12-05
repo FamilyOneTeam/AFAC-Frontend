1c2195cf1235b3bf91e3e3dc3e700cdab41c3479
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-05 17:58:41 +0300

    feat: login page

---
+++ src/features/auth/login/page.tsx
@@ -0,0 +1,26 @@
+import AuthLayout from "@/common/layouts/auth-layout";
+import LoginForm from "./form";
+
+function App() {
+  return (
+    <>
+      <AuthLayout
+        image={
+          <img
+            className="h-[95%] rounded w-full object-cover"
+            src="/auth/login-image.svg"
+            alt="bienvenido/a"
+          />
+        }
+        content={
+          <div className="pr-0 md:pr-12">
+            <h1 className="text-3xl">Ingresa tus credenciales</h1>
+            <LoginForm />
+          </div>
+        }
+      ></AuthLayout>
+    </>
+  );
+}
+
+export default App;
