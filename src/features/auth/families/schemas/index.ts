6f1f3f210ee83c459f2669b6ea35879e0d212ca9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-17 17:25:11 +0300

    feat:forgot and reset password schemas

--- src/features/auth/families/schemas/index.ts
+++ src/features/auth/families/schemas/index.ts
@@ -49,3 +49,39 @@ z.object({
       });
     }
 });
+
+export const forgotPasswordSchema = z.object({
+  correo: z.string().email({message: 'Introduce un correo valido'}),
+});
+
+export const resetPasswordSchema = z.object({
+  contrasena: z
+    .string()
+    .min(8, {
+      message: 'Una contraseña tiene que tener un minimo de 8 caracteres',
+    })
+    .max(25, {
+      message: 'Una contraseña no puede tener que tener más que 25 caracteres',
+    })
+    .regex(/^[a-zA-Z\d_-]{8,}$/, {
+      message: 'Password can only contain letters, digits, _, and -',
+    }),
+    repiteContrasena: z.string()
+    .min(8, {
+      message: 'Una contraseña tiene que tener un minimo de 8 caracteres',
+    })
+    .max(25, {
+      message: 'Una contraseña no puede tener que tener más que 25 caracteres',
+    })
+    .regex(/^[a-zA-Z\d_-]{8,}$/, {
+      message: 'Password can only contain letters, digits, _, and -',
+    }),
+}).superRefine((val, ctx) => {
+  if (val.contrasena !== val.repiteContrasena) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'La contraseña y la confirmación deben ser las mismas',
+        path: ['repiteContrasena'],
+      })
+    }
+});
