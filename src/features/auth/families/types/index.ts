4079fc6c893295fe95ddc69c7bf49622f6b28142
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-17 17:25:18 +0300

    feat:forgot and reset password schemas types

--- src/features/auth/families/types/index.ts
+++ src/features/auth/families/types/index.ts
@@ -1,4 +1,6 @@
 import { z } from 'zod';
-import { updateCredencialsFamiliesSchema } from '../schemas/index';
+import { forgotPasswordSchema, resetPasswordSchema, updateCredencialsFamiliesSchema } from '../schemas/index';
 
 export type updateCredencialsFamiliesSchemaType = z.infer<typeof updateCredencialsFamiliesSchema>;
+export type forgotPasswordSchemaType = z.infer<typeof forgotPasswordSchema>;
+export type resetPasswordSchemaType = z.infer<typeof resetPasswordSchema>;
