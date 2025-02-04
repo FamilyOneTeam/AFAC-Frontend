f70d27a6d571391d12f84672f6bad114bf4f4fc7
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-04 23:19:38 +0300

    feat: no follow ups compnent

---
+++ src/features/admin/follow-up/components/no-follow-up.tsx
@@ -0,0 +1,15 @@
+
+export default function NoFollowUp() {
+    return (
+      <div className="bg-white p-16 text-center flex flex-col gap-2 mt-12">
+        <h2 className="text-redNormal text-lg md:text-xl font-bold">
+          Aún no hay Nuevos Seguimientos
+        </h2>
+        <p className="font-semibold">¡Mantén el vínculo fuerte!</p>
+        <p className="text-BgDarker text-sm">
+          Da seguimiento a las familias de acogida y asegúrate de que cuenten
+          con el apoyo necesario para marcar la diferencia.
+        </p>
+      </div>
+    );
+}
