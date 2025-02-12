2568d08d1e95bbb63ab335fe9decd3d30272c0ec
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-13 01:17:59 +0300

    style: updated

--- src/features/welcome/page.tsx
+++ src/features/welcome/page.tsx
@@ -1,25 +1,27 @@
-import { Button } from "@/components/ui";
 import { Link } from "react-router-dom";
 
 export default function WelcomeScreen() {
     return (
       <>
         <section className="grid items-center">
-          <div className="relative flex flex-col gap-12 md:gap-16">
-            <img
-              src="/common/logo-AFAC.svg"
-              alt="AFAC"
-              className="max-w-36 -mb-4 md:-mb-6"
-            />
-            <h1 className=" font-normal text-2xl md:text-4xl">
+          <div className="relative flex flex-col gap-14 md:gap-16">
+            <Link to={'/'}>
+              <img
+                src="/common/logo-AFAC.svg"
+                alt="AFAC"
+                className="max-w-36 -mb-10"
+              />
+            </Link>
+            <h1 className="font-normal text-2xl md:text-4xl">
               Bienvenido al Sistema <br /> de Gestión de Datos <br />
               de AFAC
             </h1>
-            <Button variant={"primary"} className="w-full md:w-52">
-              <Link to={"/auth/login"} className="text-center w-full">
-                Empzar
-              </Link>
-            </Button>
+            <Link
+              to={"/auth/login"}
+              className="text-center w-full md:w-52 rounded-lg  px-4 py-3 font-normal bg-greenNormal focus:bg-greenNormal focus:border focus:border-black active:bg-white active:text-grayDarker active:border active:border-greenDarker disabled:bg-greenLight disabled:text-gray-900 focus:text-black text-black hover:bg-greenDarker hover:text-white"
+            >
+              Empzar
+            </Link>
           </div>
         </section>
         <section className="items-center hidden md:grid">
