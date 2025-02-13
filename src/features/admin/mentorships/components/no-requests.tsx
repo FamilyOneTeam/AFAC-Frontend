0eb5d7cf3b862b2d93998ce0748d4001f14a305f
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-13 20:33:06 +0300

    feat: no requests-found component

---
+++ src/features/admin/mentorships/components/no-requests.tsx
@@ -0,0 +1,15 @@
+
+export default function NoMentorshipsRequests() {
+  return (
+    <div className="bg-white p-16 text-center flex flex-col gap-2 mt-12">
+      <h2 className="text-redNormal text-lg md:text-xl font-bold">
+        Aún no hay Mentorías Agendadas
+      </h2>
+      <p className="font-semibold">¡Inspira y guía a más familias!</p>
+      <p className="text-BgDarker text-sm">
+        Comparte el poder de la experiencia fomentando mentorías que transformen
+        vidas.
+      </p>
+    </div>
+  );
+}
