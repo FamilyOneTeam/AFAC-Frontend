da1d94b186292770f8d4f5ffdd4f4d1ac79ef6d4
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-19 18:05:19 +0300

    chore: list family route

--- src/features/admin/dashboard/components/general-database.tsx.tsx
+++ src/features/admin/dashboard/components/general-database.tsx.tsx
@@ -8,7 +8,7 @@ export default function GeneralDatabase() {
         <p>Base de Datos General</p>
       </div>
       <div className=" h-[2px] mt-2 w-full bg-yellowNormal mb-2"></div>
-      <Link to={""} className="flex items-center gap-1 p-2">
+      <Link to={"/families-list"} className="flex items-center gap-1 p-2">
         <img src="/dashboard/home/listado.svg" alt="" className="w-8" />
         <span className=" text-lg font-bold">Listado de Familias</span>
       </Link>
